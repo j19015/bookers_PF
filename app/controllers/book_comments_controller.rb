3a681234c10f5931fd66a5f0d935456ac4af6531
j19015 <j19015@sangi.jp>
2022-09-30 10:06:52 +0900

    book comment asynchronous complete ver1.0

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,11 @@
 class BookCommentsController < ApplicationController
   def create
     BookComment.create(book_id: params[:book_id],user_id: current_user.id,comment: params[:book_comment]["comment"])
-    redirect_to request.referer
+    @book=Book.find(params[:book_id])
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @book=Book.find(params[:book_id])
   end
 end
