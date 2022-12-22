dce5fedbb6e96633d8a9026d8d5f537921c26273
j19015 <j19015@sangi.jp>
2022-12-23 01:32:58 +0900

    chat layout ver1.0

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :follow_check,only:[:message]
+  before_action :check,only:[:message]
   def message
     @message=Message.new
     @send_messages=Message.where(send_user_id:current_user.id,receive_user_id:params[:id])
@@ -8,9 +8,15 @@ class MessagesController < ApplicationController
   def create
     @message=Message.new(message_params)
     @message.send_user_id=current_user.id
-    @message.save!
-    @send_messages=Message.where(send_user_id:current_user.id,receive_user_id:message_params[:receive_user_id])
-    @receive_messages=Message.where(send_user_id:message_params[:receive_user_id],receive_user_id:current_user.id)
+    if @message.save!
+      @send_messages=Message.where(send_user_id:current_user.id,receive_user_id:message_params[:receive_user_id])
+      @receive_messages=Message.where(send_user_id:message_params[:receive_user_id],receive_user_id:current_user.id)
+    else
+      @message=Message.new
+      @send_messages=Message.where(send_user_id:current_user.id,receive_user_id:params[:id])
+      @receive_messages=Message.where(send_user_id:params[:id],receive_user_id:current_user.id)
+      redirect_back fallback_location: root_path
+    end
   end
 
   private
@@ -18,7 +24,7 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:receive_user_id,:chat)
   end
 
-  def follow_check
+  def check
       ##自分自身とのDMを防ぐ
       if params[:id]==current_user.id
         redirect_back fallback_location: root_path
