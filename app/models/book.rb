5fd0c604e0e4f9346cc18463388d9faef3324fb0
j19015 <j19015@sangi.jp>
2022-12-29 20:48:01 +0900

    7b complete

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,17 @@ class Book < ApplicationRecord
   validates :body,presence: true
   validates :body,length: {maximum: 200}
 
+  #本日の投稿数
+  scope :created_today,->{where(created_at: Time.zone.now.all_day).count}
+  #昨日の投稿数
+  scope :created_yesterday,->{where(created_at: 1.day.ago.all_day).count}
+  #今週の投稿数
+  scope :created_this_week,->{where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day).count}
+  #先週の投稿数
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day).count}
+  
+  
+  
   def favorite_by?(user,book)
     favorites.exists?(user_id: user.id,book_id: book.id)
   end
