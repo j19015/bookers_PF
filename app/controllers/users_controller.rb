d26db3e99ece4524fb9626b7f2d049dcbac62bea
j19015 <j19015@sangi.jp>
2023-01-12 10:17:24 +0900

    add guest action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :currect_user,only:[:edit, :update]
+  before_action :ensure_guest_user, only: [:edit]
   def index
     @users=User.all
     @user=current_user
@@ -36,4 +37,12 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user.id)
     end
   end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end  
+  
 end
