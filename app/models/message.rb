05c32d3465ddbcdddf3972264210378b1010167e
j19015 <j19015@sangi.jp>
2023-01-13 10:27:57 +0900

    repaire message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :send_user, class_name: 'User'
   belongs_to :receive_user, class_name: 'User'
   validates :chat, presence: true
-  validates :caht, length: { maximum: 140 }
+  validates :chat, length: { maximum: 140 }
 end
